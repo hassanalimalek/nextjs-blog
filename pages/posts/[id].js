71ffdeaefe2a1855a1361fec953fa050729ae9e2
Hassan Ali <hassan.infinitybits@gmail.com>
2022-04-30 21:41:32 +0500

    Layout changes

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,6 +1,6 @@
 import Layout from '../../components/layout'
 import Head from 'next/head'
-import Date from '../../components/date'
+// import Date from '../../components/date'
 import { getAllPostIds, getPostData } from '../../lib/posts'
 import utilStyles from '../../styles/utils.module.css'
 
@@ -30,7 +30,7 @@ export default function Post ({ postData }) {
       <article>
         <h1 className={utilStyles.headingXl}>{postData.title}</h1>
         <div className={utilStyles.lightText}>
-          <Date dateString={postData.date} />
+          {/* <Date dateString={postData.date} /> */}
         </div>
         <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
       </article>
